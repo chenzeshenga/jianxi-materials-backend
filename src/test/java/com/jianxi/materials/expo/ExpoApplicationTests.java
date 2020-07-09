495b349a65191d230067ea1e39c942c114812415
chenzeshenga <chenzeshenga@163.com>
2020-07-09 22:00:26 +0800

    first commit

---
+++ src/test/java/com/jianxi/materials/expo/ExpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jianxi.materials.expo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
