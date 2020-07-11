167b2f1b763992a8ef621100ed6cafcbe7f7e111
chenzeshenga <chenzeshenga@163.com>
2020-07-11 23:27:09 +0800

    test controller

--- src/main/java/com/jianxi/materials/expo/controller/TestController.java
+++ src/main/java/com/jianxi/materials/expo/controller/TestController.java
@@ -1,12 +1,16 @@
 package com.jianxi.materials.expo.controller;
 
 import com.jianxi.materials.expo.mapper.TestMapper;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -16,7 +20,11 @@ public class TestController {
   @Resource private TestMapper testMapper;
 
   @GetMapping("/list")
-  public List<String> get() {
+  public List<String> get(
+      HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+    String currentOrigin = httpServletRequest.getHeader("Origin");
+    httpServletResponse.setHeader("Access-Control-Allow-Origin", currentOrigin);
+    httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
     return testMapper.get();
   }
 }
